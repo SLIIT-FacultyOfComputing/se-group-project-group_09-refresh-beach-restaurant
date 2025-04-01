aaf9ebd7f90e5c18c45edf510e43b73a60de97d6
LINASH <it23442566@my.sliit.lk>
2025-04-01 19:24:59 +0530

    changed

--- restaurant2/src/main/java/grp_9/restaurant2/repository/MenuItemRepository.java
+++
@@ -1,7 +0,0 @@
-package grp_9.restaurant2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import grp_9.restaurant2.entity.MenuItem;
-
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
-}
