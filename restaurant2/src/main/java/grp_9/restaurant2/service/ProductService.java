27ab910cfde12bfd1b348338989f54f1add314f1
LINASH <it23442566@my.sliit.lk>
2025-05-07 18:06:08 +0530

    service

---
+++ restaurant2/src/main/java/grp_9/restaurant2/service/ProductService.java
@@ -0,0 +1,11 @@
+package grp_9.restaurant2.service;
+
+
+
+import grp_9.restaurant2.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+}
